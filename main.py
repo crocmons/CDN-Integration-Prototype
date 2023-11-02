823d21b6bb6b5c1ac9f28bbff99542fb24fbb3ca
Your Name <you@example.com>
2023-11-02 19:45:24 +0600

    updated code with id

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
 import os
+import uuid  # Import the UUID module
 
-# Kong base URL (replace with my Kong configuration)
+# Kong base URL (replace with your Kong configuration)
 KONG_BASE_URL = "http://localhost:8001"
 
-# Define the route path (demo route)
+# Define the route path (replace with your actual route)
 ROUTE_PATH = "/my-route"
 
 # Define the asset storage directory (for simulation)
@@ -14,30 +15,43 @@ ASSET_STORAGE_DIR = "assets/"
 if not os.path.exists(ASSET_STORAGE_DIR):
     os.makedirs(ASSET_STORAGE_DIR)
 
+def generate_unique_id():
+    # Generate a unique ID for each asset using UUID
+    return str(uuid.uuid4())
+
 def upload_asset(file_path):
     try:
+        # Generate a unique ID for the asset
+        asset_id = generate_unique_id()
+
         # Simulate asset upload by copying the file to the asset storage directory
         file_name = os.path.basename(file_path)
-        asset_path = os.path.join(ASSET_STORAGE_DIR, file_name)
+        asset_path = os.path.join(ASSET_STORAGE_DIR, asset_id + "_" + file_name)
         with open(file_path, 'rb') as src_file, open(asset_path, 'wb') as dest_file:
             dest_file.write(src_file.read())
 
         # Generate a mock CDN URL for the uploaded asset
-        cdn_url = f"{KONG_BASE_URL}{ROUTE_PATH}/assets/{file_name}"
+        cdn_url = f"{KONG_BASE_URL}{ROUTE_PATH}/assets/{asset_id}/{file_name}"
         return cdn_url
     except Exception as e:
         print(f"Asset upload failed: {str(e)}")
         return None
 
-def retrieve_asset(asset_filename):
+def retrieve_asset(asset_id, asset_filename):
     try:
         # Simulate asset retrieval by returning a mock CDN URL
-        cdn_url = f"{KONG_BASE_URL}{ROUTE_PATH}/assets/{asset_filename}"
+        cdn_url = f"{KONG_BASE_URL}{ROUTE_PATH}/assets/{asset_id}/{asset_filename}"
         return cdn_url
     except Exception as e:
         print(f"Asset retrieval failed: {str(e)}")
         return None
 
-# Example:
-mock_retrieve_url = retrieve_asset('asset.ext')
-print(f"Retrieved Asset URL: {mock_retrieve_url}")
+# Example usage:
+# Replace 'file_path' and 'asset_filename' with your specific values.
+asset_id = generate_unique_id()
+upload_url = upload_asset('images/hacktoberfest.png')
+print(f"Uploaded Asset ID: {asset_id}")
+
+# Later, when retrieving the asset, provide both the asset_id and the asset filename:
+retrieval_url = retrieve_asset(asset_id, 'hacktoberfest.png')  # Provide both asset_id and asset_filename
+print(f"Retrieved Asset URL: {retrieval_url}")
